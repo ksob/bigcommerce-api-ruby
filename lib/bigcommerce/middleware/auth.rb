c3c95866897c707d66d5dc496b6ee1071b1ac1ae
dev <dev@dev>
2025-12-02 15:15:11 +0100

    Fix BigCommerce Auth middleware to properly handle Faraday v2 compatibility by implementing on_request and ensuring headers are set correctly. (saragabrielinc/bigcommerce-api-ruby)

--- lib/bigcommerce/middleware/auth.rb
+++ lib/bigcommerce/middleware/auth.rb
@@ -1,9 +1,18 @@
 module Bigcommerce
   module Middleware
     class Auth < Faraday::Middleware
+      X_AUTH_CLIENT_HEADER = 'X-Auth-Client'
+      X_AUTH_TOKEN_HEADER = 'X-Auth-Token'
+      
+      def initialize(app, options = {})
+        super(app)
+        @options = options
+      end
+
       def on_request(env)
-        env[:request_headers][X_AUTH_CLIENT_HEADER] = options[:client_id]
-        env[:request_headers][X_AUTH_TOKEN_HEADER] = options[:access_token]
+        env[:request_headers][X_AUTH_CLIENT_HEADER] = @options[:client_id]
+        env[:request_headers][X_AUTH_TOKEN_HEADER] = @options[:access_token]
+        @app.call(env)
       end
     end
   end
